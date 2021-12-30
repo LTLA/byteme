f7c06d1999057aea41d57cb7898d96abb46d4f7f
LTLA <LTLA@users.noreply.github.com>
2021-12-30 04:55:03 +0000

    Deploying to gh-pages from @ clusterfork/buffin@af2aac7a9cb3733d02944ec32a6f71e7641ba02c 🚀

--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['parse_5fgzip_5ffile_2ehpp_0',['parse_gzip_file.hpp',['../parse__gzip__file_8hpp.html',1,'']]],
-  ['parse_5ftext_5fbuffer_2ehpp_1',['parse_text_buffer.hpp',['../parse__text__buffer_8hpp.html',1,'']]],
-  ['parse_5ftext_5ffile_2ehpp_2',['parse_text_file.hpp',['../parse__text__file_8hpp.html',1,'']]],
-  ['parse_5fzlib_5fbuffer_2ehpp_3',['parse_zlib_buffer.hpp',['../parse__zlib__buffer_8hpp.html',1,'']]]
+  ['parse_5finput_5fstream_2ehpp_1',['parse_input_stream.hpp',['../parse__input__stream_8hpp.html',1,'']]],
+  ['parse_5ftext_5fbuffer_2ehpp_2',['parse_text_buffer.hpp',['../parse__text__buffer_8hpp.html',1,'']]],
+  ['parse_5ftext_5ffile_2ehpp_3',['parse_text_file.hpp',['../parse__text__file_8hpp.html',1,'']]],
+  ['parse_5fzlib_5fbuffer_2ehpp_4',['parse_zlib_buffer.hpp',['../parse__zlib__buffer_8hpp.html',1,'']]]
 ];
