8f5e2207dd5f2104049f30f99cbdabaef15027ea
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-01-28 17:26:35 +0000

    Deploying to gh-pages from @ clusterfork/buffin@4bf69ed16ec53b927db38ceeeba6af89fcf554bd 🚀

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -4,10 +4,12 @@ var searchData=
   ['parse_5fgzip_5ffile_2ehpp_1',['parse_gzip_file.hpp',['../parse__gzip__file_8hpp.html',1,'']]],
   ['parse_5finput_5fstream_2',['parse_input_stream',['../parse__input__stream_8hpp.html#a52842516521ba02f60416c466dd60b91',1,'buffin']]],
   ['parse_5finput_5fstream_2ehpp_3',['parse_input_stream.hpp',['../parse__input__stream_8hpp.html',1,'']]],
-  ['parse_5ftext_5fbuffer_4',['parse_text_buffer',['../parse__text__buffer_8hpp.html#a53faf87683e7376bef037e33a053383f',1,'buffin']]],
-  ['parse_5ftext_5fbuffer_2ehpp_5',['parse_text_buffer.hpp',['../parse__text__buffer_8hpp.html',1,'']]],
-  ['parse_5ftext_5ffile_6',['parse_text_file',['../parse__text__file_8hpp.html#a112ed2e3fdbc23d57e717a35558edf1b',1,'buffin']]],
-  ['parse_5ftext_5ffile_2ehpp_7',['parse_text_file.hpp',['../parse__text__file_8hpp.html',1,'']]],
-  ['parse_5fzlib_5fbuffer_8',['parse_zlib_buffer',['../parse__zlib__buffer_8hpp.html#aa33f6ab792e0fce165656af7e5d29b93',1,'buffin']]],
-  ['parse_5fzlib_5fbuffer_2ehpp_9',['parse_zlib_buffer.hpp',['../parse__zlib__buffer_8hpp.html',1,'']]]
+  ['parse_5fsome_5ffile_4',['parse_some_file',['../parse__some__file_8hpp.html#a18032fe88d57bea90c83194ed3f5b4b5',1,'buffin']]],
+  ['parse_5fsome_5ffile_2ehpp_5',['parse_some_file.hpp',['../parse__some__file_8hpp.html',1,'']]],
+  ['parse_5ftext_5fbuffer_6',['parse_text_buffer',['../parse__text__buffer_8hpp.html#a53faf87683e7376bef037e33a053383f',1,'buffin']]],
+  ['parse_5ftext_5fbuffer_2ehpp_7',['parse_text_buffer.hpp',['../parse__text__buffer_8hpp.html',1,'']]],
+  ['parse_5ftext_5ffile_8',['parse_text_file',['../parse__text__file_8hpp.html#a112ed2e3fdbc23d57e717a35558edf1b',1,'buffin']]],
+  ['parse_5ftext_5ffile_2ehpp_9',['parse_text_file.hpp',['../parse__text__file_8hpp.html',1,'']]],
+  ['parse_5fzlib_5fbuffer_10',['parse_zlib_buffer',['../parse__zlib__buffer_8hpp.html#aa33f6ab792e0fce165656af7e5d29b93',1,'buffin']]],
+  ['parse_5fzlib_5fbuffer_2ehpp_11',['parse_zlib_buffer.hpp',['../parse__zlib__buffer_8hpp.html',1,'']]]
 ];
--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['parse_5fgzip_5ffile_0',['parse_gzip_file',['../parse__gzip__file_8hpp.html#a48b95c554a0e13778d39a4a2dfaf67de',1,'buffin']]],
   ['parse_5finput_5fstream_1',['parse_input_stream',['../parse__input__stream_8hpp.html#a52842516521ba02f60416c466dd60b91',1,'buffin']]],
-  ['parse_5ftext_5fbuffer_2',['parse_text_buffer',['../parse__text__buffer_8hpp.html#a53faf87683e7376bef037e33a053383f',1,'buffin']]],
-  ['parse_5ftext_5ffile_3',['parse_text_file',['../parse__text__file_8hpp.html#a112ed2e3fdbc23d57e717a35558edf1b',1,'buffin']]],
-  ['parse_5fzlib_5fbuffer_4',['parse_zlib_buffer',['../parse__zlib__buffer_8hpp.html#aa33f6ab792e0fce165656af7e5d29b93',1,'buffin']]]
+  ['parse_5fsome_5ffile_2',['parse_some_file',['../parse__some__file_8hpp.html#a18032fe88d57bea90c83194ed3f5b4b5',1,'buffin']]],
+  ['parse_5ftext_5fbuffer_3',['parse_text_buffer',['../parse__text__buffer_8hpp.html#a53faf87683e7376bef037e33a053383f',1,'buffin']]],
+  ['parse_5ftext_5ffile_4',['parse_text_file',['../parse__text__file_8hpp.html#a112ed2e3fdbc23d57e717a35558edf1b',1,'buffin']]],
+  ['parse_5fzlib_5fbuffer_5',['parse_zlib_buffer',['../parse__zlib__buffer_8hpp.html#aa33f6ab792e0fce165656af7e5d29b93',1,'buffin']]]
 ];
