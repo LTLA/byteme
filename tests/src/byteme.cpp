93582a9af3d0897025181435b1bec95b1e182230
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-04-15 18:32:58 -0700

    Added explicit tests for magic numbers, removed unnecessary templating.

--- tests/src/byteme.cpp
+++ tests/src/byteme.cpp
@@ -7,7 +7,7 @@
 
 #include "zlib.h"
 
-TEST(UmbrellaTest, Basic) {
+TEST(Miscellaneous, Umbrella) {
     std::vector<std::string> contents { "wakatte iru wa yo", "deai no shunkan ni", "ase ga nijinda Babyface" };
     std::string stuff;
     for (const auto& x : contents) {
@@ -22,4 +22,36 @@ TEST(UmbrellaTest, Basic) {
     byteme::GzipFileReader reader(gzname.c_str(), {});
     auto lines = read_lines(reader);
     EXPECT_EQ(lines, contents);
+}
+
+TEST(Miscellaneous, MagicNumbers) {
+    unsigned char zlib_header[2] { 0x78, 0x01 };
+    EXPECT_TRUE(byteme::is_zlib(zlib_header, 2));
+    zlib_header[1] = 0x5e;
+    EXPECT_TRUE(byteme::is_zlib(zlib_header, 2));
+    zlib_header[1] = 0x9c;
+    EXPECT_TRUE(byteme::is_zlib(zlib_header, 2));
+    zlib_header[1] = 0xda;
+    EXPECT_TRUE(byteme::is_zlib(zlib_header, 2));
+    zlib_header[1] = 0x20;
+    EXPECT_TRUE(byteme::is_zlib(zlib_header, 2));
+    zlib_header[1] = 0x7d;
+    EXPECT_TRUE(byteme::is_zlib(zlib_header, 2));
+    zlib_header[1] = 0xbb;
+    EXPECT_TRUE(byteme::is_zlib(zlib_header, 2));
+    zlib_header[1] = 0xf9;
+    EXPECT_TRUE(byteme::is_zlib(zlib_header, 2));
+    EXPECT_FALSE(byteme::is_zlib(zlib_header, 1));
+    zlib_header[1] = 0x00;
+    EXPECT_FALSE(byteme::is_zlib(zlib_header, 2));
+    zlib_header[0] = 0x00;
+    EXPECT_FALSE(byteme::is_zlib(zlib_header, 2));
+
+    unsigned char gzip_header[2] { 0x1f, 0x8b };
+    EXPECT_FALSE(byteme::is_gzip(gzip_header, 1));
+    EXPECT_TRUE(byteme::is_gzip(gzip_header, 2));
+    gzip_header[1] = 0x00;
+    EXPECT_FALSE(byteme::is_gzip(gzip_header, 2));
+    gzip_header[0] = 0x00;
+    EXPECT_FALSE(byteme::is_gzip(gzip_header, 2));
 }
