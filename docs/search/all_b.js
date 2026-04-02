53ebd60a7dcf96671db89b5351f7dc509adf2d22
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-04-02 02:58:23 +0000

    Deploying to gh-pages from @ LTLA/byteme@879556da8400329936c30a029df945d28064e459 🚀

--- docs/search/all_b.js
+++ docs/search/all_b.js
@@ -14,6 +14,7 @@ var searchData=
   ['read_11',['read',['../classbyteme_1_1GzipFileReader.html#a4e293b837f53b17cbdaac22e0305d669',1,'byteme::GzipFileReader::read()'],['../classbyteme_1_1IstreamReader.html#aced8bbf4560953a5c331c6ec18edd742',1,'byteme::IstreamReader::read()'],['../classbyteme_1_1RawBufferReader.html#aa2ed5c3a15c37cb69e0ad6a6141f6441',1,'byteme::RawBufferReader::read()'],['../classbyteme_1_1RawFileReader.html#a42ba2360c659f3365a79d0654e26f737',1,'byteme::RawFileReader::read()'],['../classbyteme_1_1Reader.html#ac39e5bfe2feae64643a177ba7dcb7fa9',1,'byteme::Reader::read()'],['../classbyteme_1_1ZlibBufferReader.html#a932af82edf6c8d316f0fb9b54d032179',1,'byteme::ZlibBufferReader::read()']]],
   ['reader_12',['Reader',['../classbyteme_1_1Reader.html',1,'byteme']]],
   ['reader_2ehpp_13',['Reader.hpp',['../Reader_8hpp.html',1,'']]],
-  ['reserve_14',['reserve',['../structbyteme_1_1RawBufferWriterOptions.html#ace854857d6b1b607ff4fedca07d05e1b',1,'byteme::RawBufferWriterOptions::reserve'],['https://ltla.github.io/sanisizer/namespacesanisizer.html#a1a07e85a7c7d29d992442f2d40476188',1,'sanisizer::reserve(Container_ &amp;container, Value_ x, Args_ &amp;&amp;... args)']]],
-  ['resize_15',['resize',['https://ltla.github.io/sanisizer/namespacesanisizer.html#a3df6e5117dcf28cab0e705d4ac499f0d',1,'sanisizer']]]
+  ['required_5fbits_5ffor_5ffloat_14',['required_bits_for_float',['https://ltla.github.io/sanisizer/namespacesanisizer.html#a714d5385b83631d2aace857ca004fee8',1,'sanisizer']]],
+  ['reserve_15',['reserve',['../structbyteme_1_1RawBufferWriterOptions.html#ace854857d6b1b607ff4fedca07d05e1b',1,'byteme::RawBufferWriterOptions::reserve'],['https://ltla.github.io/sanisizer/namespacesanisizer.html#a1a07e85a7c7d29d992442f2d40476188',1,'sanisizer::reserve(Container_ &amp;container, Value_ x, Args_ &amp;&amp;... args)']]],
+  ['resize_16',['resize',['https://ltla.github.io/sanisizer/namespacesanisizer.html#a3df6e5117dcf28cab0e705d4ac499f0d',1,'sanisizer']]]
 ];
--- docs/search/functions_a.js
+++ docs/search/functions_a.js
@@ -5,6 +5,7 @@ var searchData=
   ['rawfilereader_2',['RawFileReader',['../classbyteme_1_1RawFileReader.html#a8408ecd4c0c0d0c03d84b205f941f8d2',1,'byteme::RawFileReader']]],
   ['rawfilewriter_3',['RawFileWriter',['../classbyteme_1_1RawFileWriter.html#a85b813251f5ce733364d3b02914ad209',1,'byteme::RawFileWriter']]],
   ['read_4',['read',['../classbyteme_1_1GzipFileReader.html#a4e293b837f53b17cbdaac22e0305d669',1,'byteme::GzipFileReader::read()'],['../classbyteme_1_1IstreamReader.html#aced8bbf4560953a5c331c6ec18edd742',1,'byteme::IstreamReader::read()'],['../classbyteme_1_1RawBufferReader.html#aa2ed5c3a15c37cb69e0ad6a6141f6441',1,'byteme::RawBufferReader::read()'],['../classbyteme_1_1RawFileReader.html#a42ba2360c659f3365a79d0654e26f737',1,'byteme::RawFileReader::read()'],['../classbyteme_1_1Reader.html#ac39e5bfe2feae64643a177ba7dcb7fa9',1,'byteme::Reader::read()'],['../classbyteme_1_1ZlibBufferReader.html#a932af82edf6c8d316f0fb9b54d032179',1,'byteme::ZlibBufferReader::read()']]],
-  ['reserve_5',['reserve',['https://ltla.github.io/sanisizer/namespacesanisizer.html#a1a07e85a7c7d29d992442f2d40476188',1,'sanisizer']]],
-  ['resize_6',['resize',['https://ltla.github.io/sanisizer/namespacesanisizer.html#a3df6e5117dcf28cab0e705d4ac499f0d',1,'sanisizer']]]
+  ['required_5fbits_5ffor_5ffloat_5',['required_bits_for_float',['https://ltla.github.io/sanisizer/namespacesanisizer.html#a714d5385b83631d2aace857ca004fee8',1,'sanisizer']]],
+  ['reserve_6',['reserve',['https://ltla.github.io/sanisizer/namespacesanisizer.html#a1a07e85a7c7d29d992442f2d40476188',1,'sanisizer']]],
+  ['resize_7',['resize',['https://ltla.github.io/sanisizer/namespacesanisizer.html#a3df6e5117dcf28cab0e705d4ac499f0d',1,'sanisizer']]]
 ];
